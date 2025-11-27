402fe185eac809103a32301161fd301874be9f34
Ania-sm <aniasami161007@gmail.com>
2025-11-27 17:33:34 +0100

    Navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,25 @@
 import '../index.css';
+
+function Navbar_item({link,text}){
+    return(
+        <li>
+            <a href={link} className='text-white hover:text-[#D6BC61] transition-colors relative after:content-[""] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-[#D6BC61] after:transition-all after:duration-300 hover:after:w-full'>
+                {text}
+            </a>
+        </li>
+    )
+}
+
 function Navbar(){
     return (
-    <nav className="fixed top-0 left-0 right-0 w-full bg-black">
+    <nav className="fixed top-0 left-0 right-0 pb-3 pt-3 w-full bg-black">
         <ul className="flex flex-row gap-8 m-0 justify-center ">
-            <li><a href="#home" className='text-white hover:underline hover:text-[#D6BC61]'>HOME</a></li>
-            <li><a href="#about" className='text-white hover:underline hover:text-[#D6BC61]' >ABOUT US</a></li>
-            <li><a href="#mentors" className='text-white hover:underline hover:text-[#D6BC61]' >MENTORS</a></li>
-            <li><a href="#agenda" className="text-white hover:underline hover:text-[#D6BC61]" >AGENDA</a></li>
-            <li><a href="#objectives" className="text-white hover:underline hover:text-[#D6BC61]" >OBJECTIVES</a></li>
-            <li><a href="#contacts" className="text-white hover:underline hover:text-[#D6BC61]" >CONTACTS</a></li>
+            <Navbar_item link="#home" text="HOME"/>
+            <Navbar_item link="#about" text="ABOUT US"/>
+            <Navbar_item link="#mentors" text="MENTORS"/>
+            <Navbar_item link="#agenda" text="AGENDA"/>
+            <Navbar_item link="#objectives" text="OBJECTIVES"/>
+            <Navbar_item link="#contacts" text="CONTACTS"/>
         </ul>
     </nav>
     )
